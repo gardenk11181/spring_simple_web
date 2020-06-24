ad358c2e1233fa9f2f3a75aaafeafc964281e85c
gardenk11181 <55073876+gardenk11181@users.noreply.github.com>
2020-06-24 18:41:50 +0900

    Initial Commit

---
+++ src/main/java/com/example/simpleweb/SpringSimpleWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.simpleweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSimpleWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSimpleWebApplication.class, args);
+	}
+
+}
